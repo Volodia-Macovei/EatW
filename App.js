313100f06b52459ccb5adca12b9ae7b8d6c5ba01
Volodia Macovei <volodia.macovei@gmail.com>
2022-03-06 20:08:14 +0200

    status bar andr

--- App.js
+++ App.js
@@ -1,19 +1,18 @@
 import React from 'react';
-import { StatusBar } from 'expo-status-bar';
+import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { SafeAreaView, Platform } from 'react-native';
+import { SafeAreaView, Platform, StatusBar } from 'react-native';
 
 export default function App() {
   return (
     <>
-    <SafeAreaView style = {{ flex: 1 }}>
+    <SafeAreaView style = {{ flex: 1, marginTop: StatusBar.currentHeight }}>
     <View style ={{ padding: 20, backgroundColor: 'green' }}>
       <Text>search</Text>
-      <StatusBar style="auto" />
     </View>
     <View style = {{ flex: 1,padding: 20, backgroundColor: 'blue' }}>
     <Text>list</Text>
-    <StatusBar style="auto" />
+    <ExpoStatusBar style="auto" />
   </View>
   </SafeAreaView>
   </>
